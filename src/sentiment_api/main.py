9ae661328df1614249dbd85570c7f9484a8d4412
Kushagra Tewari <kushagratewari.eng@gmail.com>
2026-03-28 13:50:13 +0530

    feat: load model once on startup using lifespan

--- src/sentiment_api/main.py
+++ src/sentiment_api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from src.sentiment_api.model import train_model, predict_sentiment
+from src.sentiment_api.model import train_model, predict_sentiment, load_model, save_model
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+ml_models = {}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    model, vectorizer = train_model()
+    save_model(model, vectorizer)
+    ml_models["model"], ml_models["vectorizer"] = load_model()
+    yield
+    ml_models.clear()
+
+app = FastAPI(lifespan = lifespan)
 
 @app.get("/health")
 def health():
@@ -13,6 +23,6 @@ class TextInput(BaseModel):
 
 @app.post("/predict")
 def predict(input: TextInput):
-    model,vectorizer = train_model()
+    model,vectorizer = ml_models["model"],ml_models["vectorizer"]
     sentiment = predict_sentiment(input.text,model,vectorizer)
     return {"sentiment": sentiment}
